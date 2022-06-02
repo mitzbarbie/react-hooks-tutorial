aa554526d041ab888333faf010c2a70092440a97
SeungHwan Hong <mitzbarbie@naver.com>
2022-06-02 18:11:39 +0900

    useEffect

--- src/Info.js
+++ src/Info.js
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Info = () => {
   const [name, setName] = useState("");
   const [nickname, setNickname] = useState("");
+
+  useEffect(() => {
+    console.log("testtest");
+  }, []);
 
   const onChangeName = (e) => {
     setName(e.target.value);
