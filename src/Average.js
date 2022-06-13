8e73be824d016fa1906672917a3b43c27867a47c
SeungHwan Hong <mitzbarbie@naver.com>
2022-06-13 15:51:14 +0900

    tiny fixed

--- src/Average.js
+++ src/Average.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const getAverage = (numbers) => {
-  console.log("counting get average...");
+  console.log("get counting average...");
   if (numbers.length === 0) return 0;
   const sum = numbers.reduce((a, b) => a + b);
   return sum / numbers.length;
