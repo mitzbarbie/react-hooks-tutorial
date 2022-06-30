21ec90058ed8f47363318eba998d6fb5fc0ac35b
SeungHwan Hong <mitzbarbie@naver.com>
2022-06-30 11:08:09 +0900

    custom hooks

--- src/Info.js
+++ src/Info.js
@@ -1,45 +1,72 @@
 import { useEffect, useState } from "react";
+import useInputs from "./useInput";
+
+// const Info = () => {
+//   const [name, setName] = useState("");
+//   const [nickname, setNickname] = useState("");
+
+//   useEffect(() => {
+//     console.log("effect");
+//     console.log(name);
+//     return () => {
+//       console.log("cleanup");
+//       console.log(name);
+//     };
+//   }, [name]);
+
+//   const onChangeName = (e) => {
+//     setName(e.target.value);
+//   };
+
+//   const onChangeNickname = (e) => {
+//     setNickname(e.target.value);
+//   };
+
+//   return (
+//     <div>
+//       <div>
+//         <input
+//           value={name}
+//           onChange={onChangeName}
+//           placeholder="write your name"
+//         />
+//         <input
+//           value={nickname}
+//           onChange={onChangeNickname}
+//           placeholder="write your nickname"
+//         />
+//       </div>
+//       <div>
+//         <b>이름:</b> {name}
+//       </div>
+//       <div>
+//         <b>닉네임:</b> {nickname}
+//       </div>
+//     </div>
+//   );
+// };
 
 const Info = () => {
-  const [name, setName] = useState("");
-  const [nickname, setNickname] = useState("");
+  const [state, onChange] = useInputs({
+    name: "",
+    nickname: "",
+  });
 
-  useEffect(() => {
-    console.log("effect");
-    console.log(name);
-    return () => {
-      console.log("cleanup");
-      console.log(name);
-    };
-  }, [name]);
-
-  const onChangeName = (e) => {
-    setName(e.target.value);
-  };
-
-  const onChangeNickname = (e) => {
-    setNickname(e.target.value);
-  };
+  const { name, nickname } = state;
 
   return (
     <div>
       <div>
-        <input
-          value={name}
-          onChange={onChangeName}
-          placeholder="write your name"
-        />
-        <input
-          value={nickname}
-          onChange={onChangeNickname}
-          placeholder="write your nickname"
-        />
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
       </div>
       <div>
-        <b>이름:</b> {name}
-      </div>
-      <div>
-        <b>닉네임:</b> {nickname}
+        <div>
+          <b>name:</b> {name}
+        </div>
+        <div>
+          <b>nickname:</b> {nickname}
+        </div>
       </div>
     </div>
   );
